31400566a4a48e3aa6fa0650311ed0ee58a3a2a8
Kenny <kennymonique972@gmail.com>
2022-02-15 15:24:59 +0100

    correction de l'auth

--- src/Controller/Front/UserController.php
+++ src/Controller/Front/UserController.php
@@ -1,11 +1,13 @@
 <?php
 
-namespace App\Controller\Admin;
+namespace App\Controller\Front;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
 
 class UserController extends AbstractController
 {
